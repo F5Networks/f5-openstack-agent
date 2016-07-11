8931db82bc101796bea09bbfdd2525f87a913139
Jeffrey Longstaff <j.longstaff@f5.com>
2016-07-11 13:37:46 -0600

    Update conf.py for Mitaka
    Change Liberty to Mitaka

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ release = VERSION
 
 # OpenStack release
 
-openstack_release = "Liberty"
+openstack_release = "Mitaka"
 rst_epilog = """
 .. |openstack| replace:: {0}
 """.format(openstack_release)
