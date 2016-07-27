351a717e0bbe49a69a5acde6463576e8a92d845c
richbrowne <r.browne@f5.com>
2016-07-27 08:31:13 -0600

    Saving rework for rpm builds

--- f5-openstack-agent-dist/Docker/redhat/7/build-rpms.sh
+++ f5-openstack-agent-dist/Docker/redhat/7/build-rpms.sh
@@ -1,27 +1,38 @@
+0 bash                                                                                                                                                            SEA-ML-RBROWNE  07/26  8:52pm
 #!/bin/bash -ex
 
 SRC_DIR=$1
-DIST_DIR=$2
+PKG_NAME=f5-openstack-agent
+DIST_DIR="${PKG_NAME}-dist"
+RPMBUILD_DIR="rpmbuild"
 OS_VERSION=7
 
 DEST_DIR="${SRC_DIR}/${DIST_DIR}"
 
-echo "Building RPM packages...${DIST_DIR}"
-buildroot=$(mktemp -d /tmp/f5-openstack-agent-dist.XXXX)
+echo "Building ${PKG_NAME} RPM packages..."
+buildroot=$(mktemp -d /tmp/${PKG_NAME}.XXXXX)
 
 cp -R $SRC_DIR/* ${buildroot}
 
 pushd ${buildroot}
+python setup.py build bdist_rpm --rpm-base rpmbuild
 
-python setup.py bdist_rpm
+echo "%_topdir ${buildroot}/rpmbuild" > ~/.rpmmacros
 
-mkdir -p ${DEST_DIR}/rpms
+python setup.py bdist_rpm --spec-only --dist-dir rpmbuild/SPECS
+echo "%config /etc/neutron/services/f5/f5-openstack-agent.ini" >> rpmbuild/SPECS/f5-openstack-agent.spec
+rpmbuild -ba rpmbuild/SPECS/f5-openstack-agent.spec
 
-for pkg in $(ls dist/*.rpm); do
+mkdir -p ${DEST_DIR}/rpms/build
+
+for pkg in $(ls rpmbuild/RPMS/noarch/*.rpm); do
   if [[ $pkg =~ ".noarch." ]]; then
     mv $pkg ${pkg%%.noarch.rpm}.el${OS_VERSION}.noarch.rpm
   fi
 done
-cp -R dist/*.rpm ${DEST_DIR}/rpms
+cp -R rpmbuild/RPMS/noarch/*.rpm ${DEST_DIR}/rpms/build
 
 popd
+
+rm -rf ${buildroot}
+
