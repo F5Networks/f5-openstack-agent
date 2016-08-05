da94be815ad51f907e3f9493978fdc18589d8250
richbrowne <r.browne@f5.com>
2016-08-05 09:35:03 -0600

    Adding a post-build step to test the install of Debian packages

--- f5-openstack-agent-dist/scripts/test_install.sh
+++ f5-openstack-agent-dist/scripts/test_install.sh
@@ -9,16 +9,21 @@ BUILD_CONTAINER="${OS_TYPE}${OS_VERSION}-${PKG_NAME}-pkg-tester"
 WORKING_DIR="/var/wdir"
 
 if [[ ${OS_TYPE} == "redhat" ]]; then
-	CONTAINER_TYPE="centos"
+	CONTAINER_TYPE="centos${OS_VERSION}"
 elif [[ ${OS_TYPE} == "ubuntu" ]]; then
-	CONTAINER_TYPE="ubuntu"
+	if [[ ${OS_VERSION} == "14.04" ]]; then
+		CONTAINER_TYPE="trusty"
+	else
+		echo "Only Trusty release currently supported"
+		exit 1
+	fi
 else
 	echo "Unsupported target OS (${OS_TYPE})"
 	exit 1
 fi
 
 DOCKER_DIR="${DIST_DIR}/Docker/${OS_TYPE}/install_test"
-DOCKER_FILE="${DOCKER_DIR}/Dockerfile.${CONTAINER_TYPE}${OS_VERSION}"
+DOCKER_FILE="${DOCKER_DIR}/Dockerfile.${CONTAINER_TYPE}"
 
 docker build -t ${BUILD_CONTAINER} -f ${DOCKER_FILE} ${DOCKER_DIR}
 docker run --privileged --rm -v $(pwd):${WORKING_DIR} ${BUILD_CONTAINER} /usr/bin/python \
