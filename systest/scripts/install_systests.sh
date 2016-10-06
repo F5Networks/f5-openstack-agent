76edaf7a0cb6473cf5dc6c0c7eaaf0fdb1ee9d6c
Za Wilgustus <za@F5.com>
2016-10-06 14:31:33 +0000

    Fix accidental requirements downgrade.
    This commit also abstracts the buildbot
    target branch, and moves dependency
    install logic from:
    
    `requirements.buildbot.txt`
    to:
    `systest/scripts/install_systests.sh``

--- systest/scripts/install_systests.sh
+++ systest/scripts/install_systests.sh
@@ -8,6 +8,12 @@ else
     repo="$1"
 fi
 
+if [ "$2" == "" ]; then
+    echo "ERROR: no branch value provided!"
+    exit 1
+else
+    branch="$2"
+fi
 # - work around the "unknown host" prompt
 fl=~/.ssh/config
 opt="StrictHostKeyChecking no"
@@ -21,14 +27,31 @@ pip install virtualenv
 virtualenv systest
 source systest/bin/activate
 
-# - install system 
-branch="feature.bb_integration_331"
+#Install the agent
 sudo rm -rf f5-openstack-agent
 git clone -b $branch $repo
 cd f5-openstack-agent
-pip install -r ./requirements.buildbot.txt
-cd ~
-rm -rf f5-common-python
-git clone -b development https://github.com/F5Networks/f5-common-python.git
-cd f5-common-python
 pip install --upgrade .
+
+# Install openstack deps (if necessary)
+pip install git+https://github.com/openstack/neutron#stable/$branch
+pip install git+https://github.com/openstack/oslo.log.git@stable/$branch
+pip install git+https://github.com/openstack/neutron-lbaas.git@stable/$branch
+
+# Install driver
+pip install git+https://github.com/F5Networks/f5-openstack-lbaasv2-driver.git@$branch
+
+# Install local test utils
+pip install git+https://github.com/F5Networks/f5-openstack-test.git
+pip install git+ssh://git@bldr-git.int.lineratesystems.com/tools/pytest-meta.git
+pip install git+ssh://git@bldr-git.int.lineratesystems.com/tools/pytest-symbols.git
+pip install git+ssh://git@bldr-git.int.lineratesystems.com/tools/pytest-autolog.git
+
+# Install general test utils
+pip install f5-sdk==1.5.0
+pip install mock==2.0.0
+pip install pytest==3.0.3
+pip install pytest-cov==2.3.1
+pip install responses==0.5.1
+pip install coverage==4.2
+pip install python-coveralls==2.8.0
