5f9913acb54f6ee3a449dd55a7bbb7890215472f
richbrowne <r.browne@f5.com>
2017-02-14 08:27:25 -0700

    Pool member creation fails with network_type opflex
    Issues:
    Fixes #506
    
    Problem:
    The fix for F5Networks/f5-openstack-lbaasv2-driver#353 enabled the F5
    driver/agent to detect and bind a virtual server to a dynamic VLAN
    segment, created by Cisco ACI. Now pool member creation fails because
    the agent cannot find the VLAN segment for the pool network.
    
    Need to allow member creation with missing seg ID or network segment.
    Community code already knows how to do the right thing to create a
    VLAN network segment under the opflex parent. Follow that for member
    creation. The current agent code expects the VLAN to be present prior
    to member creation. For type opflex, need to move this earlier in
    the workflow.
    
    Analysis:
    Reimplemented the support for Hierarchical(Delayed) Port Binding.
    Removed the disconnected service in favor of delaying the service
    creation until the VIP port has a fully defined network segment.
    
    Likewise, the pool member creation support is added by delaying the
    member creation on BIG-IP until a fully defined network segment
    is available in Neutron.  More details available in issue-506.
    
    Tests:
    disconnected_service/test_disconnected_service_creation.py
    loadbalancer/test_loadbalancer.py
    loadbalancer/test_loadbalancer_disconnected.py
    loadbalancer/test_loadbalancer_opflex.py
    loadbalancer/test_loadbalancer_snat.py
    member/test_single_member_opflex.py

---
+++ test/functional/neutronless/member/test_single_member_opflex.py
@@ -0,0 +1,93 @@
+# coding=utf-8
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from copy import deepcopy
+from f5_openstack_agent.lbaasv2.drivers.bigip.resource_helper import \
+    ResourceType
+import json
+import logging
+import os
+import pytest
+import requests
+import urllib
+
+from ..testlib.resource_validator import ResourceValidator
+from ..testlib.service_reader import LoadbalancerReader
+
+
+requests.packages.urllib3.disable_warnings()
+
+LOG = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="module")
+def services():
+    neutron_services_filename = (
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                     '../../testdata/service_requests/create_delete_opflex_member.json')
+    )
+    return (json.load(open(neutron_services_filename)))
+
+
+@pytest.fixture()
+def icd_config():
+    oslo_config_filename = (
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                     '../../config/basic_agent_config.json')
+    )
+    OSLO_CONFIGS = json.load(open(oslo_config_filename))
+
+    config = deepcopy(OSLO_CONFIGS)
+    config['icontrol_hostname'] = pytest.symbols.bigip_mgmt_ip_public
+    config['icontrol_username'] = pytest.symbols.bigip_username
+    config['icontrol_password'] = pytest.symbols.bigip_password
+    config['f5_vtep_selfip_name'] = pytest.symbols.f5_vtep_selfip_name
+
+    return config
+
+
+def test_lbaas_service_one_opflex_member(request,
+                                    bigip,
+                                    services,
+                                    icd_config,
+                                    icontrol_driver):
+    env_prefix = icd_config['environment_prefix']
+    service_iter = iter(services)
+    service = service_iter.next()
+    lb_reader = LoadbalancerReader(service)
+    fake_rpc = icontrol_driver.plugin_rpc
+    folder = '{0}_{1}'.format(env_prefix, lb_reader.tenant_id())
+
+    lb_pending = icontrol_driver._common_service_handler(service)
+    assert bigip.folder_exists(folder)
+    assert not lb_pending
+    # Assert that update loadbalancer status was called once
+    assert fake_rpc.get_call_count('update_loadbalancer_status') == 1
+
+    # create member, node
+    service = service_iter.next()
+    member = service['members'][0]
+    lb_pending = icontrol_driver._common_service_handler(service)
+    assert lb_pending
+    # Assert that update loadbalancer status was called once
+    assert fake_rpc.get_call_count('update_loadbalancer_status') == 1    
+
+    # create member, node
+    service = service_iter.next()
+    member = service['members'][0]
+    lb_pending = icontrol_driver._common_service_handler(service)
+    assert not lb_pending
+    # Assert that update loadbalancer status was called once
+    assert fake_rpc.get_call_count('update_loadbalancer_status') == 2
