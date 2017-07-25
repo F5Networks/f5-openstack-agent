ed88fdc8260dd147068a078fc452ddecd4072dc0
Jodie Putrino <j.putrino@f5.com>
2017-07-25 13:36:11 -0600

    make linkcheck exit with warning, not failure

--- docs/scripts/test-docs.sh
+++ docs/scripts/test-docs.sh
@@ -3,9 +3,15 @@
 set -x
 set -e
 
-echo "Building docs and Checking links with Sphinx"
+echo "Building docs with Sphinx"
 make -C docs html
-make -C docs linkcheck
 
 echo "Checking grammar and style"
 write-good `find ./docs -not \( -path ./docs/drafts -prune \) -name '*.rst'` --passive --so --no-illusion --thereIs --cliches
+
+set +e
+echo "Checking links"
+make -C docs linkcheck
+
+echo "The following links are broken:"
+grep "broken" docs/_build/linkcheck/output.txt
