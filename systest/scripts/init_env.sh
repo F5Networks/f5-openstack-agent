bfd0315c4cc011bb3b561469139fda30b6c2889f
Za Wilgustus <za@f5.com>
2017-09-25 15:27:42 -0600

    reconfigure to allow combined coverage reports
    * Jenkins, via its file, uses init_env.sh to set up appropriate coverage
    results reporting directories, e.g.:
    /testlab/openstack/testresults/coverage/agent/mitaka/1be3f/12.1.2-overcloud-132
    where subdirectories of "coverage" are read as:
    CI_PROJECT/CI_BRANCH/PROJ_HASH/BUILD_NAME
    
    * Makefile supports new Jenkinsfile-centric unit-test run, in a
    back-compatible way
    * unit_test_run_wrapper.sh integrates with init_env.sh, and
    record_results.sh
    * tox file no longer changesdirectories in the singlebigip job
    * requirements.unittest.txt integrates with constraints properly
    * alter tox unit to run from {toxinidir}

--- systest/scripts/init_env.sh
+++ systest/scripts/init_env.sh
@@ -12,6 +12,7 @@ job_dirname="${CI_PROGRAM}.${CI_PROJECT}.${CI_BRANCH}.${JOB_BASE_NAME}"
 export build_dirname="${JOB_BASE_NAME}-${BUILD_ID}"
 export CI_RESULTS_DIR="/home/jenkins/results/${job_dirname}/${build_dirname}"
 export CI_BUILD_SUMMARY="${CI_RESULTS_DIR}/ci-build.yaml"
+export PYTHONDONTWRITEBYTECODE=1
 
 # The following logic enables combined coverage reporting.
 covsuffix="${CI_PROJECT}/${CI_BRANCH}/${PROJ_HASH}/${build_dirname}"
