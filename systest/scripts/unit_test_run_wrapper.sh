d9adc569bd90e775dd2b1b71d2f0c77427292b9c
Za Wilgustus <za@f5.com>
2017-09-28 12:13:25 -0600

    fix unitest .coverage report path

--- systest/scripts/unit_test_run_wrapper.sh
+++ systest/scripts/unit_test_run_wrapper.sh
@@ -9,4 +9,4 @@ sudo -E docker run -u jenkins -v `pwd`:/home/jenkins/f5-openstack-agent \
 docker-registry.pdbld.f5net.com/openstack-test-agentunitrunner-prod/mitaka \
 $STAGENAME $SESSIONLOGDIR
 mkdir -p ${COVERAGERESULTS}-unit
-mv f5_openstack_agent/.coverage ${COVERAGERESULTS}-unit
+mv .coverage ${COVERAGERESULTS}-unit
