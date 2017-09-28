a57c0cbb31b5be6504f2f4fb686de4dd6b36ee20
Za Wilgustus <za@f5.com>
2017-09-28 12:38:04 -0600

    avoid use of sudo

--- systest/scripts/unit_test_run_wrapper.sh
+++ systest/scripts/unit_test_run_wrapper.sh
@@ -5,8 +5,8 @@ export TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 export SESSIONLOGDIR=${TAGINFO}_$TIMESTAMP
 
 export STAGENAME=f5-openstack-agent_mitaka-unit
-sudo -E cd /home/jenkins/f5-openstack-agent && \
-docker run -u jenkins -v `pwd`:/home/jenkins/f5-openstack-agent \
+cd /home/jenkins/f5-openstack-agent && \
+sudo -E docker run -u jenkins -v `pwd`:/home/jenkins/f5-openstack-agent \
 docker-registry.pdbld.f5net.com/openstack-test-agentunitrunner-prod/mitaka \
 $STAGENAME $SESSIONLOGDIR
 mkdir -p ${COVERAGERESULTS}-unit
