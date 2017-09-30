b85f15911ae88e5b18113274c301b0275dfbfc93
Paul Breaux <p.breaux@f5.com>
2017-09-30 16:48:52 -0600

    Fixing references to mitaka containers for unit tests. Should be newton containers.

--- systest/scripts/unit_test_run_wrapper.sh
+++ systest/scripts/unit_test_run_wrapper.sh
@@ -4,12 +4,12 @@ export TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 # This is approximately the same as GUMBALLS_SESSION
 export SESSIONLOGDIR=${TAGINFO}_$TIMESTAMP
 
-export STAGENAME=f5-openstack-agent_mitaka-unit
+export STAGENAME=f5-openstack-agent_newton-unit
 pwd
 ls -l
-sudo -E docker pull  docker-registry.pdbld.f5net.com/openstack-test-agentunitrunner-prod/mitaka
+sudo -E docker pull  docker-registry.pdbld.f5net.com/openstack-test-agentunitrunner-prod/newton
 sudo -E docker run -u jenkins -v `pwd`:/home/jenkins/f5-openstack-agent \
-docker-registry.pdbld.f5net.com/openstack-test-agentunitrunner-prod/mitaka:latest \
+docker-registry.pdbld.f5net.com/openstack-test-agentunitrunner-prod/newton:latest \
 $STAGENAME $SESSIONLOGDIR
 mkdir -p ${COVERAGERESULTS}-unit
 mv .coverage ${COVERAGERESULTS}-unit
