2347fd00dd9290dad486f3b094351635fe465e53
Za Wilgustus <za@f5.com>
2017-10-10 13:30:49 -0600

    remove debug prints

--- systest/scripts/record_results.sh
+++ systest/scripts/record_results.sh
@@ -4,8 +4,6 @@ set -ex
 
 # - copy results files to nfs (note that the nfs results directory is mounted
 #    inside the CI worker's home directory)
-echo INSIDE RECORD RESULTS!!
-echo CI_RESULTS_DIR is: ${CI_RESULTS_DIR}
 mkdir -p $CI_RESULTS_DIR/
 cp -r $WORKSPACE/systest/test_results/* $CI_RESULTS_DIR/
 mkdir -p ${COVERAGERESULTS}
