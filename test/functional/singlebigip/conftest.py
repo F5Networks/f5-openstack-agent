57a87c7a93f0e46c0cdfc59badf27a66fa9672cf
Steven Sorenson <s.sorenson@f5.com>
2018-01-31 13:38:03 -0700

    Consolidated names as they were expected in enviro
    Issues:
    
    Problem:
    * Tests expect the default 'bigip1' hostname, which is edited for tests
    
    Analysis:
    * Changes hostname for BIG-IP to what the test environment expectes
    
    Tests:
    This was tested in a newton funct-test environment with a 11.6 BIG-IP

--- test/functional/singlebigip/conftest.py
+++ test/functional/singlebigip/conftest.py
@@ -3,8 +3,13 @@ import pytest
 from f5.bigip import ManagementRoot
 from pytest import symbols
 
+
 @pytest.fixture(scope="module")
-def mgmt_root ():
-    m_obj = ManagementRoot(symbols.bigip_mgmt_ip_public, symbols.bigip_username, symbols.bigip_password)
-    m_obj._meta_data['device_name'] = 'bigip1'
+def mgmt_root():
+    bigip_quantname = \
+        "host-{s.bigip_mgmt_ip}.openstacklocal".format(s=symbols)
+    m_obj = ManagementRoot(
+        symbols.bigip_mgmt_ip_public, symbols.bigip_username,
+        symbols.bigip_password)
+    m_obj._meta_data['device_name'] = bigip_quantname
     return m_obj
