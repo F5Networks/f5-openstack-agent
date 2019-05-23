4120263cd9558802e772aa4530631fcd98900e9e
Andrew Zong <a.zong@f5.com>
2019-05-23 12:02:11 +0800

    Add password_cipher_mode to encrypt password.

--- f5_openstack_agent/lbaasv2/drivers/bigip/agent_manager.py
+++ f5_openstack_agent/lbaasv2/drivers/bigip/agent_manager.py
@@ -129,6 +129,11 @@ OPTS = [
         default=60,
         help=(
             'Amount of time to wait for a errored service to become active')
+    ),
+    cfg.BoolOpt(
+        'password_cipher_mode',
+        default=False,
+        help='The flag indicating the password is plain text or not.'
     )
 ]
 
