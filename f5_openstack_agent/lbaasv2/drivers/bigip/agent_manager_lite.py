92691476ef4b1d300d82ed8b6cbf9c8268897d3d
Peter Pzhang <peter.zhang@f5.com>
2021-01-07 19:58:50 -0800

    Add avaliability_zone configuration for agenting state reporting
    (cherry picked from commit 3d963162a8c2271e230de832de3c5ef48bcc2606)

--- f5_openstack_agent/lbaasv2/drivers/bigip/agent_manager_lite.py
+++ f5_openstack_agent/lbaasv2/drivers/bigip/agent_manager_lite.py
@@ -201,7 +201,8 @@ class LbaasAgentManager(periodic_task.PeriodicTasks):  # b --> B
             'agent_type': constants_v2.F5_AGENT_TYPE_LOADBALANCERV2,
             'l2_population': self.conf.l2_population,
             'start_flag': True,
-            'configurations': agent_configurations
+            'configurations': agent_configurations,
+            'availability_zone': self.conf.availability_zone
         }
 
         # Setup RPC for communications to and from controller
