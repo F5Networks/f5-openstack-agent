da44fca61ffdefddd023de20b0aeb4462e9d2636
Qin Zhao <q.zhao@f5.com>
2021-01-16 11:34:16 +0800

    Ensure to overwrite persistence profile

--- f5_openstack_agent/lbaasv2/drivers/bigip/resource_manager.py
+++ f5_openstack_agent/lbaasv2/drivers/bigip/resource_manager.py
@@ -392,7 +392,7 @@ class ListenerManager(ResourceManager):
         }
         super(ListenerManager, self)._create(
             bigip, payload, None, None, type="http-cookie",
-            helper=self.http_cookie_persist_helper, overwrite=False)
+            helper=self.http_cookie_persist_helper)
         return name
 
     def _create_source_addr_persist_profile(self, bigip, vs, persist):
@@ -408,7 +408,7 @@ class ListenerManager(ResourceManager):
         }
         super(ListenerManager, self)._create(
             bigip, payload, None, None, type="source-addr",
-            helper=self.source_addr_persist_helper, overwrite=False)
+            helper=self.source_addr_persist_helper)
         return name
 
     def _create_ssl_profile(self, bigip, vs, tls):
