e5b63a3b93af8ec7a6007291f9c7036f88626e0f
Seven Zhang <hu.zhang@f5.com>
2022-08-21 23:26:28 +0800

    [OPENSTACK-2531] update exception msg

--- f5_openstack_agent/client/bigip.py
+++ f5_openstack_agent/client/bigip.py
@@ -12,7 +12,6 @@ from clientmanager import IControlClient
 
 LOG = logging.getLogger(__name__)
 
-
 INDENT = 4
 INVENTORY_PATH = '/etc/neutron/bigip_inventory.json'
 
@@ -52,19 +51,22 @@ class BipipCommand(command.ShowOne):
     def get_blob(self, group_id):
         data = self._load_inventory()
         if group_id not in data:
-            msg = (_("id: %(id) not in inventory") % {'id': group_id})
+            msg = _("id: %s not in inventory" % group_id)
             raise exceptions.CommandError(msg)
         return data[group_id]
 
     def show_inventory(self, group_id=None):
-        if group_id:
-            blob = self.get_blob(group_id)
-            return (('ID', 'GROUP'),
-                    (group_id, json.dumps(blob, indent=INDENT)))
-        else:
-            inventory = self._load_inventory()
-            return (('ID', 'GROUP'),
-                    ((k, json.dumps(v, indent=INDENT)) for k, v in inventory.items()))
+        blob = self.get_blob(group_id)
+        return (('ID', 'GROUP'),
+                (group_id, json.dumps(blob, indent=INDENT)))
+
+    def delete_group(self, group_id):
+        inventory = self._load_inventory()
+        if group_id not in inventory:
+            msg = _("id: %s not in inventory" % group_id)
+            raise exceptions.CommandError(msg)
+        del inventory[group_id]
+        self._write_inventory(inventory)
 
 
 class CreateBigip(BipipCommand):
@@ -148,24 +150,30 @@ class DeleteBigip(BipipCommand):
         )
 
         parser.add_argument(
-            'icontrol_hostname',
+            '--icontrol_hostname',
             metavar='<icontrol_hostname>',
+            default=None,
             help=_('icontrol_hostname of BIG-IP device'),
         )
         return parser
 
     def take_action(self, parsed_args):
-        blob = self.get_blob(parsed_args.id)
-        bigips = blob.get("bigips", None)
-        icontrol_hostname = parsed_args.icontrol_hostname
-        if icontrol_hostname not in bigips:
-            msg = (_("bigip: %(hostname)s not in group") % {'hostname': icontrol_hostname})
-            raise exceptions.CommandError(msg)
-        del bigips[icontrol_hostname]
-        blob['bigips'] = bigips
-        self.update_bigip(parsed_args.id, blob)
+        if parsed_args.icontrol_hostname:
+            blob = self.get_blob(parsed_args.id)
+            bigips = blob.get("bigips", None)
+            icontrol_hostname = parsed_args.icontrol_hostname
+            if icontrol_hostname not in bigips:
+                msg = _("bigip: %s not in group" % icontrol_hostname)
+                raise exceptions.CommandError(msg)
+            del bigips[icontrol_hostname]
+            blob['bigips'] = bigips
+            self.update_bigip(parsed_args.id, blob)
 
-        return self.show_inventory(parsed_args.id)
+            return self.show_inventory(parsed_args.id)
+        else:
+            self.delete_group(parsed_args.id)
+            msg = 'delete group: %s successful' % parsed_args.id
+            return (('MESSAGE'), (msg))
 
 
 class UpdateBigip(BipipCommand):
@@ -227,7 +235,7 @@ class RefreshBigip(BipipCommand):
         bigips = blob.get("bigips", None)
         icontrol_hostname = parsed_args.icontrol_hostname
         if icontrol_hostname not in bigips:
-            msg = (_("bigip: %(hostname) not in group") % {'hostname': icontrol_hostname})
+            msg = _("bigip: %s not in group" % icontrol_hostname)
             raise exceptions.CommandError(msg)
 
         bigip_info = bigips[icontrol_hostname]
@@ -238,11 +246,15 @@ class RefreshBigip(BipipCommand):
         return self.show_inventory(parsed_args.id)
 
 
-class ListBigip(BipipCommand):
+class ListBigip(command.Lister):
     _description = _("List all BIG-IP in the inventory")
 
     def take_action(self, parsed_args):
-        return self.show_inventory()
+        with open(INVENTORY_PATH, 'r') as f:
+            inventory = json.load(f)
+        return (('ID', 'GROUP'),
+                ((k, json.dumps(v, indent=INDENT)) for k, v in inventory.items())
+                )
 
 
 class ShowBigip(BipipCommand):
