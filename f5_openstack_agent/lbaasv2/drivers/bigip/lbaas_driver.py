99ab0cdf421bc9e26ea749ce6be4cc206d7af6fd
pzhang <pzhang@f5.com>
2022-08-23 09:04:15 +0000

    OPENSTACK-2533: remove code from icontrol and agent_manager

--- f5_openstack_agent/lbaasv2/drivers/bigip/lbaas_driver.py
+++ f5_openstack_agent/lbaasv2/drivers/bigip/lbaas_driver.py
@@ -67,98 +67,8 @@ class LBaaSBaseDriver(object):
         """Update pool member operational status from devices to controller."""
         raise NotImplementedError()
 
-    def recover_errored_devices(self):
-        """Trigger attempt to reconnect any errored devices."""
-        raise NotImplementedError()
-
     def get_stats(self, service):
         """Get Stats for a loadbalancer Service."""
-        raise NotImplementedError()
-
-    def get_all_deployed_loadbalancers(self, purge_orphaned_folders=True):
-        """Get all Loadbalancers defined on devices."""
-        raise NotImplementedError()
-
-    def purge_orphaned_loadbalancer(self, tenant_id, loadbalancer_id,
-                                    hostnames):
-        """Remove all loadbalancers without references in Neutron."""
-        raise NotImplementedError()
-
-    def service_exists(self, service):
-        """Check If LBaaS Service is Defined on Driver Target."""
-        raise NotImplementedError()
-
-    def sync(self, service):
-        """Force Sync a Service on Driver Target."""
-        raise NotImplementedError()
-
-    def create_pool(self, pool, service):
-        """LBaaS Create Pool."""
-        raise NotImplementedError()
-
-    def update_pool(self, old_pool, pool, service):
-        """LBaaS Update Pool."""
-        raise NotImplementedError()
-
-    def delete_pool(self, pool, service):
-        """LBaaS Delete Pool."""
-        raise NotImplementedError()
-
-    def create_member(self, member, service, the_port_id=None):
-        """LBaaS Create Member."""
-        raise NotImplementedError()
-
-    def update_member(self, old_member, member, service):
-        """LBaaS Update Member."""
-        raise NotImplementedError()
-
-    def delete_member(self, member, service):
-        """LBaaS Delete Member."""
-        raise NotImplementedError()
-
-    def create_pool_health_monitor(self, health_monitor, pool, service):
-        """LBaaS Create Pool Health Monitor."""
-        raise NotImplementedError()
-
-    def update_health_monitor(self, old_health_monitor,
-                              health_monitor,
-                              pool,
-                              service):
-        """LBaaS Update Health Monitor."""
-        raise NotImplementedError()
-
-    def delete_health_monitor(self, health_monitor, pool, service):
-        """LBaaS Delete Health Monior."""
-        raise NotImplementedError()
-
-    def delete_pool_health_monitor(self, health_monitor, pool, service):
-        """LBaaS Delete Health Monitor."""
-        raise NotImplementedError()
-
-    def get_all_deployed_health_monitors(self):
-        """Get listing of all deployed Health Monitors"""
-        raise NotImplementedError()
-
-    def purge_orphaned_health_monitor(self, tenant_id=None, monitor_id=None,
-                                      hostnames=list()):
-        """LBaaS Purge Health Monitor."""
-        raise NotImplementedError()
-
-    def get_all_deployed_l7_policys(self):
-        """Get listing of all deployed Health Monitors"""
-        raise NotImplementedError()
-
-    def purge_orphaned_l7_policy(self, tenant_id=None, monitor_id=None,
-                                 hostnames=list()):
-        """LBaaS Purge Health Monitor."""
-        raise NotImplementedError()
-
-    def tunnel_update(self, **kwargs):
-        """Neutron Core Tunnel Update."""
-        raise NotImplementedError()
-
-    def tunnel_sync(self):
-        """Neutron Core Tunnel Sync Messages."""
         raise NotImplementedError()
 
     def fdb_add(self, fdb_entries):
@@ -171,28 +81,4 @@ class LBaaSBaseDriver(object):
 
     def fdb_update(self, fdb_entries):
         """L2 Population FDB Update."""
-        raise NotImplementedError()
-
-    def create_l7policy(self, l7policy, service):
-        """LBaaS Create l7policy."""
-        raise NotImplementedError()
-
-    def update_l7policy(self, old_l7policy, l7policy, service):
-        """LBaaS Update l7policy."""
-        raise NotImplementedError()
-
-    def delete_l7policy(self, l7policy, service):
-        """LBaaS Delete l7policy."""
-        raise NotImplementedError()
-
-    def create_l7rule(self, l7rule, service):
-        """LBaaS Create l7rule."""
-        raise NotImplementedError()
-
-    def update_l7rule(self, old_l7rule, l7rule, service):
-        """LBaaS Update l7rule."""
-        raise NotImplementedError()
-
-    def delete_l7rule(self, l7rule, service):
-        """LBaaS Delete l7rule."""
         raise NotImplementedError()
